ad2452d6f8aa964ce4586bee8a601233b945a5ca
Mikael Hartzell <freelcs1@gmail.com>
2018-05-13 20:39:37 +0300

    Added formats: 'mov','3gp','3g2','mj2' to nonfree mpeg formats, that gets enables if the user enables mp4. All these formats are based on mp4.

--- installer.py
+++ installer.py
@@ -5740,7 +5740,7 @@ global_mxf_audio_remix_channel_map = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
 # The value of ['all'] means allow all ffmpeg supported formats to be processed.
 # Use only lower case characters for the format names.
 mxf_formats = ['mxf','mxf_d10']
-mpeg_nonfree_wrapper_formats = ['mp4','m4v','m4a']
+mpeg_nonfree_wrapper_formats = ['mp4','m4v','m4a','mov','3gp','3g2','mj2']
 ffmpeg_free_wrapper_formats = ['wav','flac','ogg','mkv','matroska','mka','mpeg','mp2','mp3','mpegts','mpegtsraw','mpegvideo','mpeg1video','mpeg2video','vcd','svcd','dvd','vob','ac3']
 ffmpeg_allowed_wrapper_formats = ['all']
 enable_all_nonfree_ffmpeg_wrapper_formats = tkinter.BooleanVar()
